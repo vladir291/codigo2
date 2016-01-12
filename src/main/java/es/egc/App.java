f8c7c114920ad4dd3a3c082ed99c4174c3b32303
abc <a@b.c>
2016-01-12 09:31:48 +0100

    Estructura
    Commit inicial

---
+++ src/main/java/es/egc/App.java
@@ -0,0 +1,22 @@
+package es.egc;
+import org.apache.log4j.*; 
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static Logger log=Logger.getLogger(App.class); 
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	public int suma(int a, int b){
+		int result= a-b;
+		log.info("Returning "+a+"+"+b+"="+result);
+		return result;
+	}
+}
